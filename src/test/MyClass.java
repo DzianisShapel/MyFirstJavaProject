24d10b2933b0c405ed8dbc9b404e3977915e81f8
Dzianis Shapel <dzianis283@gmail.com>
2022-06-19 19:35:35 +0300

    19.06.2022 movie 34

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -2,29 +2,44 @@ package test;
 
 public class MyClass {
 
-// i - here is a class level variable (instance variable)
-    int i;
+    int i = 5;
+    String str = "Hello World";
 
-//method  with the name of class name. It means it is a constructor
     public MyClass(){
-        i = 6;
-    }
-/*we can create multiple constructors in  a particular class
-It is possible because we are using different arguments
- */
-    public MyClass(int i){
-            this.i=i;
+
     }
 
     public static void main(String[] args) {
-    /*create a new instance of particular class,
-    and we can use class object and access the variable*/
-        MyClass myClass = new MyClass(7);
-//        myClass.initialize();
-        System.out.println(myClass.i);
+        MyClass myClass = new MyClass();
+        myClass.stringCheck();
+
     }
 
-    public void initialize(){
-            i = 5;
+    public MyClass initialize(){
+        i = 6;
+        return this;
     }
+
+    public void stringCheck(){
+        str = str.concat(" !!");
+        System.out.println(str);
+        str = str.toUpperCase();
+        System.out.println(str);
+        /** str = str.substring(6);
+        System.out.println(str);*/
+        str = str.substring(6, 11);
+        System.out.println(str);
+        boolean flag = str.equalsIgnoreCase("WORLD");
+        System.out.println(flag);
+
+        str = "5";
+        int i = Integer.parseInt(str);
+        System.out.println(i);
+
+        i = 6;
+        str = String.valueOf(i);
+        System.out.println(i);
+
+    }
+
 }
