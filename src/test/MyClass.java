36c64fdb5e46225bc4b5042360c564d7e4241bfa
Dzianis Shapel <dzianis283@gmail.com>
2022-06-20 22:31:49 +0300

    inheritance

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -2,44 +2,15 @@ package test;
 
 public class MyClass {
 
-    int i = 5;
-    String str = "Hello World";
-
-    public MyClass(){
-
-    }
-
     public static void main(String[] args) {
-        MyClass myClass = new MyClass();
-        myClass.stringCheck();
-
+        MySubClass mySubClass = new MySubClass("constructor string from myClass");
+        mySubClass.superClassMethod();
+        System.out.println(mySubClass.superStr);
+        mySubClass.subClassMethod();
+        mySubClass.printCommonString();
+        mySubClass.printCommonMethod();
     }
 
-    public MyClass initialize(){
-        i = 6;
-        return this;
-    }
 
-    public void stringCheck(){
-        str = str.concat(" !!");
-        System.out.println(str);
-        str = str.toUpperCase();
-        System.out.println(str);
-        /** str = str.substring(6);
-        System.out.println(str);*/
-        str = str.substring(6, 11);
-        System.out.println(str);
-        boolean flag = str.equalsIgnoreCase("WORLD");
-        System.out.println(flag);
-
-        str = "5";
-        int i = Integer.parseInt(str);
-        System.out.println(i);
-
-        i = 6;
-        str = String.valueOf(i);
-        System.out.println(i);
-
-    }
 
 }
--- src/test/MyClass1.java
+++ src/test/MyClass1.java
@@ -2,9 +2,44 @@ package test;
 
 public class MyClass1 {
 
+    /**int i = 5;
+    String str = "Hello World";
+
+    public MyClass(){}
+
+
+
     public void myMethod1(){
         MyClass myClass = new MyClass();
         myClass.initialize();
         System.out.println(myClass.i);
     }
+
+    public MyClass initialize(){
+        i = 6;
+        return this;
+    }
+
+    public void stringCheck(){
+        str = str.concat(" !!");
+        System.out.println(str);
+        str = str.toUpperCase();
+        System.out.println(str);
+        /** str = str.substring(6);
+         System.out.println(str);
+        str = str.substring(6, 11);
+        System.out.println(str);
+        boolean flag = str.equalsIgnoreCase("WORLD");
+        System.out.println(flag);
+
+        str = "5";
+        int i = Integer.parseInt(str);
+        System.out.println(i);
+
+        i = 6;
+        str = String.valueOf(i);
+        System.out.println(i);
+
+    }*/
 }
+
